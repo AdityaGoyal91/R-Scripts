c30bb777440e63cba75f4f911f3c0981538d8a27
AdityaGoyal91 <AdityaGoyal91@users.noreply.github.com>
2017-09-26 18:53:25 -0700

    missing line of code added
    was missing the line of code to subset the data over time. Added it in.

--- DraftVectorCreation.R
+++ DraftVectorCreation.R
@@ -1,24 +1,28 @@
 ##### Draft Vector Creation #####
 ## Create a vector to simulate a draft order based on how many picks each player gets.
 ## First, make sure the data set has the following columns:
-## 1. user id - some id to make sure we know who gets each pick.
+## 1. userid - some id to make sure we know who gets each pick.
 ## 2. rank - the order in which each person will get to pick.
 ## 3. count - the number of picks each person gets.
 
 draftVector <- function(data) {
     vectorOP <- c()
-    data <- data[order(data[ , "rank"], decreasing = FALSE)]
+    data <- data[order(data[ , "rank"], decreasing = FALSE), ]
     
     while(sum(data[ , "count"]) > 0) {
+        
+        data <- data[data[ , "count"] > 0, ]
+        
         repVec <- as.vector(
             rep(
-                data[ , "user id"],
-                length.out = min(data[ , "count"])*nrow(data)
+                data[ , "userid"],
+                length.out = (min(data[ , "count"])*nrow(data))
             )
         )
         
         vectorOP <- c(vectorOP, repVec)
         data[ , "count"] <- data[ , "count"] - min(data[ , "count"])
+        
     }
     vectorOP
 }
