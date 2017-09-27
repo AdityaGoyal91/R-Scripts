443141f4b7e3eb1bb2fcae9e23b985f0d5f678b1
AdityaGoyal91 <AdityaGoyal91@users.noreply.github.com>
2017-09-26 19:41:08 -0700

    added serpentine draft function

--- DraftVectorCreation.R
+++ DraftVectorCreation.R
@@ -26,3 +26,41 @@ draftVector <- function(data) {
     }
     vectorOP
 }
+
+##### Serpentine Draft Vector Creation #####
+## Create a vector to simulate a draft order based on how many picks each player gets.
+## First, make sure the data set has the following columns:
+## 1. userid - some id to make sure we know who gets each pick.
+## 2. rank - the order in which each person will get to pick.
+## 3. count - the number of picks each person gets.
+
+serpdraftVector <- function(data) {
+    
+    c = 0
+    vectorOP <- c()
+    
+    while(sum(data[ , "count"]) > 0) {
+        
+        c = c + 1
+        
+        data <- data[data[ , "count"] > 0, ]
+        
+        if(c %% 2 == 1) {
+            data <- data[order(data[ , "rank"], decreasing = FALSE), ]
+        } else {
+            data <- data[order(data[ , "rank"], decreasing = TRUE), ]
+        }
+        
+        repVec <- as.vector(
+            rep(
+                data[ , "userid"],
+                length.out = nrow(data)
+            )
+        )
+        
+        vectorOP <- c(vectorOP, repVec)
+        data[ , "count"] <- data[ , "count"] - 1
+        
+    }
+    vectorOP
+}
