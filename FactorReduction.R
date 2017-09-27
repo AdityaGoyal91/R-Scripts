01cd816b242d82786a8ef974e6db988dadf9a42e
AdityaGoyal91 <AdityaGoyal91@users.noreply.github.com>
2017-09-26 21:48:58 -0700

    Reduce the number of levels within a factor
    This code is to help reduce sparcity within a matrix to help the model actually run and make more sense of small observation counts by bucketing them together.

---
+++ FactorReduction.R
@@ -0,0 +1,61 @@
+##### Factor Reduction Code #####
+
+## This code is to reduce the number of levels within a factor variable to help with sparcity.
+    ## we will classify them as "_other_"
+    ## Also, any missing values will be categorized as "_Unknown_"
+## 2 inputs will be needed for this code:
+    ## 1. DataSettoTrim <- data set that needs to be cleaned up
+    ## 2. numberoffactors <- how many unique levels each factor should have at max
+
+allClass <- function(x) {
+    unlist(
+        lapply(
+            unclass(x),
+            class
+        )
+    )
+}
+
+Class.List <- which(allClass(DataSettoTrim) == "factor")
+Factor.List <- match(names(Class.List, names(DataSettoTrim)))
+
+for(i in 1:length(Factor.List)) {
+    if(length(DataSettoTrim[ ,Factor.List[i]][DataSettoTrim[ ,Factor.List[i]] == ""]) > 0) {
+        DataSettoTrim[ ,Factor.List[i]][DataSettoTrim[ ,Factor.List[i]] == ""] <- "_Unknown_"
+    }    
+}
+DataSettoTrim <- droplevels(DataSettoTrim)
+
+for(j in 1:length(Factor.List)) {
+    if(length(levels(DataSettoTrim[ , Factor.List[j]])) > numberoffactors) {
+        assign(
+            paste(
+                names(DataSettoTrim)[Factor.List[j]],
+                ".keep",
+                sep = ""
+            ),
+            names(
+                sort(
+                    summary(
+                        DataSettoTrim[ , Factor.List[j]]
+                    ),
+                    decreasing = TRUE
+                )[1:numberoffactors]
+            )
+        )
+        levels(DataSettoTrim[ , Factor.List[j]]) <- c(levels(DataSettoTrim[ , Factor.List[j]]), "_Other_")
+        DataSettoTrim[, Factor.List[j]][!(
+            DataSettoTrim[ , Factor.List[j]] %in%
+            get(
+                paste(
+                    names(
+                        DataSettoTrim
+                    )[Factor.List[j]], 
+                    ".keep", 
+                    sep = ""
+                )
+            )
+        )] <- "_Other_"
+        DataSettoTrim <- droplevels(DataSettoTrim)
+    }
+}
