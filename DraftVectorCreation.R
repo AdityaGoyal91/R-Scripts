fc11cc210cfc38ba9025019978552656e8200b26
AdityaGoyal91 <AdityaGoyal91@users.noreply.github.com>
2017-09-26 19:51:13 -0700

    removed extra spaces

--- DraftVectorCreation.R
+++ DraftVectorCreation.R
@@ -10,19 +10,15 @@ draftVector <- function(data) {
     data <- data[order(data[ , "rank"], decreasing = FALSE), ]
     
     while(sum(data[ , "count"]) > 0) {
-        
         data <- data[data[ , "count"] > 0, ]
-        
         repVec <- as.vector(
             rep(
                 data[ , "userid"],
                 length.out = (min(data[ , "count"])*nrow(data))
             )
         )
-        
         vectorOP <- c(vectorOP, repVec)
         data[ , "count"] <- data[ , "count"] - min(data[ , "count"])
-        
     }
     vectorOP
 }
@@ -35,14 +31,11 @@ draftVector <- function(data) {
 ## 3. count - the number of picks each person gets.
 
 serpdraftVector <- function(data) {
-    
     c = 0
     vectorOP <- c()
     
     while(sum(data[ , "count"]) > 0) {
-        
         c = c + 1
-        
         data <- data[data[ , "count"] > 0, ]
         
         if(c %% 2 == 1) {
@@ -50,17 +43,14 @@ serpdraftVector <- function(data) {
         } else {
             data <- data[order(data[ , "rank"], decreasing = TRUE), ]
         }
-        
         repVec <- as.vector(
             rep(
                 data[ , "userid"],
                 length.out = nrow(data)
             )
         )
-        
         vectorOP <- c(vectorOP, repVec)
         data[ , "count"] <- data[ , "count"] - 1
-        
     }
     vectorOP
 }
